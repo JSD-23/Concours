4d9c8991a26e4a8a6b75564827c2cbcf03cad137
Hacprog <118378624+Hacprog@users.noreply.github.com>
2023-11-03 10:05:13 +0100

    Add files via upload

--- projet.py
+++ projet.py
@@ -1,1 +1,16 @@
-print("bonjour")
+A='bonjour bonlo'
+
+print(type(A))
+print(len(A))
+print(A[0])
+for mot in A:
+    print(len(mot))
+
+#def max_de_prix(prix,jour):
+
+   # prix=[]
+    #jour=[]
+    
+
+       
+  
